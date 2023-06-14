14faf393880751f7c5314fe1a9d41decced380e6
Jack <jack20220723@gmail.com>
2023-06-14 14:15:21 +0800

    Update copyright header (#69)

--- packages/messier-61-graph/temporary/GraphConfig.d.ts
+++ packages/messier-61-graph/temporary/GraphConfig.d.ts
@@ -1,17 +1,5 @@
 /*
- * Copyright Jiaqi Liu
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2023 Paion Data. All rights reserved.
  */
 /**
  * A graph config instructs {@link D3Graph} how the D3 network graph is going to be rendered and with what data to be
