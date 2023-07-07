dfb98060d3be7ce5f4ce38cb394acfcc351a6b23
活泼到令人头疼的胡桃 <jack20220723@gmail.com>
2023-07-07 10:57:43 +0800

    Enable sentry.io monitoring for app.nexusgraph.com (#104)
    * Enable sentry.io monitoring for app.nexusgraph.com
    
    * Self-review
    
    * Prettier
    
    * Self-review

--- packages/nexusgraph-app/src/index.tsx
+++ packages/nexusgraph-app/src/index.tsx
@@ -8,8 +8,12 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { GlobalState } from "../../nexusgraph-graph/src/shared/globalState";
 import rootReducers from "../../nexusgraph-graph/src/shared/rootReducer";
 
+import * as Sentry from "@sentry/react";
+
 const reducer = combineReducers<GlobalState>({ ...(rootReducers as any) });
 export const store = configureStore({ reducer });
+
+setupSentry();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -19,3 +23,18 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+export function setupSentry(): void {
+  Sentry.init({
+    dsn: "https://4e597714b8494ecbab3446f0347907fa@o4505480921022464.ingest.sentry.io/4505480923643904",
+    integrations: [
+      new Sentry.BrowserTracing({
+        tracePropagationTargets: ["localhost", /^https:\/\/app\.nexusgraph\.com/],
+      }),
+      new Sentry.Replay(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
